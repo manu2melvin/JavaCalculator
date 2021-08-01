9f5ba002ffdc25ab1d3ca8850491c20d00936fad
manumelvin <manumelvin@gmail.com>
2021-08-01 12:33:31 +0530

    JavaCalculator

---
+++ src/test/java/com/manu/cal/ManuCalculator/CalculatorTest.java
@@ -0,0 +1,25 @@
+package com.manu.cal.Calculator;
+
+import com.manu.cal.ManuCalculator.Calculator;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+
+    Calculator cal = new Calculator(2, 3);
+
+    @Test
+    public void sumTest() throws Exception {
+        assertEquals(5, cal.addFucn(2, 3));
+    }
+
+    @Test
+    public void differenceTest() throws Exception {
+        assertEquals(1, cal.subFucn(2, 3));
+    }
+
+    @Test
+    public void productTest() throws Exception {
+        assertEquals(6, cal.mulFucn(2, 3));
+    }
+}
